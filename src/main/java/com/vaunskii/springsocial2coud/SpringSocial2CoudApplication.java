b54f59f4577fdeafb93c2d8472af7a61b9ea0efa
v.valunskiy <v.valunskiy@okko.tv>
2023-02-25 06:24:17 +0300

    Initial commit

---
+++ src/main/java/com/vaunskii/springsocial2coud/SpringSocial2CoudApplication.java
@@ -0,0 +1,13 @@
+package com.vaunskii.springsocial2coud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocial2CoudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocial2CoudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaunskii/springsocial2coud/SpringSocial2CoudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaunskii.springsocial2coud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocial2CoudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
